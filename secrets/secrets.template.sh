0eebe5a9a5251ce3a4232c4def95b7b0c93a1b1a
Jesse Abdul <jesse.abdul@noaa.gov>
2026-05-20 19:15:45 -1000

    Updated secrets template

--- secrets/secrets.template.sh
+++ secrets/secrets.template.sh
@@ -1,21 +1,22 @@
 #--- Credentials ---
-# Set your oracle administrator password here: SYS and SYSTEM database schema passwords, APEX administrator password (workspace = INTERNAL, user = ADMIN), and the ORDS_PUBLIC_USER password
-ORACLE_PWD="YOUR_PASSWORD"
 
 # define the default admin user for Oracle
-ORACLE_ADMIN_USER=SYS
+ORACLE_ADMIN_USER="SYS"
+
+# Set your oracle administrator password here: SYS and SYSTEM database schema passwords, APEX administrator password (workspace = INTERNAL, user = ADMIN), and the ORDS_PUBLIC_USER password
+ORACLE_PWD='YOUR_PASSWORD'
 
 # define any database/apex credentials necessary to deploy the database schemas and/or applications
 
 # define DSC credentials
-DSC_USER=DSC
-DSC_PWD="YOUR_DSC_PASSWORD"
+DSC_USER="DSC"
+DSC_PWD='YOUR_DSC_PASSWORD'
 
 # define CAS credentials
-CAS_DB_USER=CAS
-CAS_DB_PWD="YOUR_CAS_USER_PASSWORD"
+CAS_DB_USER="CAS"
+CAS_DB_PWD='YOUR_CAS_USER_PASSWORD'
 
-CAS_APP_USER=CAS_APX_APP
-CAS_APP_PWD="YOUR_CAS_APP_PASSWORD"
+CAS_APP_USER="CAS_APX_APP"
+CAS_APP_PWD='YOUR_CAS_APP_PASSWORD'
 
-CAS_APX_PWD="YOUR_CAS_APEX_PASSWORD"
+CAS_APX_PWD='YOUR_CAS_APEX_PASSWORD'
