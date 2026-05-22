ada25932690e64a406f183fd49aef6ff111f1dbb
Jesse Abdul <jesse.abdul@noaa.gov>
2026-05-22 09:13:19 -1000

    Added the apex developer username as an additional secret to be specified at runtime when the container deployment scripts run

--- secrets/secrets.template.sh
+++ secrets/secrets.template.sh
@@ -12,11 +12,14 @@ ORACLE_PWD='YOUR_PASSWORD'
 DSC_USER="DSC"
 DSC_PWD='YOUR_DSC_PASSWORD'
 
-# define CAS credentials
+# define CAS data schema credentials
 CAS_DB_USER="CAS"
 CAS_DB_PWD='YOUR_CAS_USER_PASSWORD'
 
+# define CAS application schema credentials
 CAS_APP_USER="CAS_APX_APP"
 CAS_APP_PWD='YOUR_CAS_APP_PASSWORD'
 
+# define CAS apex developer account username/password
+CAS_APX_USER='CAS_APX_APP_DEV'
 CAS_APX_PWD='YOUR_CAS_APEX_PASSWORD'
